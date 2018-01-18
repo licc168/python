2085614d219d30ee9c89d5592d80a37968262268
licc168 <liccwork@126.com>
2018-01-19 07:38:38 +0800

    修改密码

--- alibaba/alibabaMain.py
+++ alibaba/alibabaMain.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import alibabaLogin
 
-browser = alibabaLogin.login("liccwork@126.com","Alicc5998012")
+browser = alibabaLogin.login("liccwork@126.com","111111111")
 request = alibabaLogin.set_sessions(browser)
 json = request.get("http://szgoldtech.en.alibaba.com/event/app/contactPerson/showContactInfo.htm?encryptAccountId=IDX1-L_yFkFFeHlkum0bTB8eAlskZoHniogxv8pKh4uAR_ZOtSg5Hrc6zYvrmUSpklIZ")
 print json.encoding
