2ebd3b41f30f53c218229760fe4398606f50e763
licc168 <liccwork@126.com>
2018-05-04 19:18:33 +0800

    1111111111111111111111111

--- start/Worldwide_start.py
+++ start/Worldwide_start.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import requests
-import time
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver import Proxy
 from selenium import webdriver
-from selenium.webdriver.common.proxy import ProxyType
-from start import IPPools
-import re
+from selenium.webdriver.firefox.options import Options
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from start import common
@@ -36,7 +32,7 @@ headers = { "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,imag
 
 
 def start():
-
+    browser=None
     while True:
         # 代理
         try:
@@ -45,7 +41,7 @@ def start():
                 ip = str(ip, encoding="utf-8")
                 if common.isUseIp(ip):
                    urls =  mysqlCli.icaUrls()
-                   mysqlCli.updateStatus()
+                   mysqlCli.updateStatus(1,0)
                    for row in urls:
                     id = row[0]
                     url = row[1]
@@ -54,55 +50,87 @@ def start():
                         try:
                             # 最大条数
                             max = row[3]
+                            #最大点赞量
+                            maxStart = row[4]
                             # 当前点赞条数
-                            count = redisClient.getSuccessStart(url)
+                            count = mysqlCli.getStartNumById(id)
 
                             if count ==None:
                                 count=0
                             count = int(count)
                             if (count>= max):
+
                                 continue
-                            chromeOptions = webdriver.ChromeOptions()
-                            chromeOptions.add_argument('--proxy-server=' + ip)
-                            prefs = {
-                               'profile.default_content_setting_values': {
-                                   'images': 2
-                               }
-                            }
-                            chromeOptions.add_experimental_option("prefs", prefs)
-                            browser = webdriver.Chrome(chrome_options=chromeOptions)
-                            browser.implicitly_wait(3)
-                            browser.set_page_load_timeout(20)
 
-                            browser.get(url)
-
-                            print(url)
-                            praisebg =  WebDriverWait(browser, 3).until(
-                                EC.presence_of_element_located((By.ID, "praisebg")))
+                           # 谷歌浏览器
+                            # chromeOptions = Options()
                             #
-                            # js = "var q=document.documentElement.scrollTop=10000"
-                            # browser.execute_script(js)
-                            time.sleep(10)
-                            # # 点赞事件
-                            praisebg.click()
-#
+                            # # chromeOptions.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+                            # chromeOptions.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+                            # chromeOptions.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+                            # # chromeOptions.add_argument('--headless')
+                            # # chromeOptions.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+                            # chromeOptions.add_argument('--proxy-server=' + ip)
+                            # # chromeOptions.add_experimental_option("prefs", prefs)
+                            # # chromeOptions.add_argument(
+                            # #     'user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36"')
+                            #
+                            # browser = webdriver.Chrome(chrome_options=chromeOptions)
+                            # browser = webdriver.Chrome()
+                            options = Options()
+                            options.add_argument('-headless')
+                            profile = webdriver.FirefoxProfile()
+                            ip_ip = ip.split(":")[0]
+                            ip_port = int(ip.split(":")[1])
+                            options.set_preference('network.proxy.type', 1)  # 默认值0，就是直接连接；1就是手工配置代理。
+                            options.set_preference('network.proxy.http', ip_ip)
+                            options.set_preference('network.proxy.http_port', ip_port)
+                            options.set_preference('network.proxy.ssl', ip_ip)
+                            options.set_preference('network.proxy.ssl_port', ip_port)
+
+                            options.set_preference("network.http.use-cache", False);
+                            options.set_preference("browser.cache.memory.enable", False);
+                            options.set_preference("browser.cache.disk.enable", False);
+                            options.set_preference("browser.sessionhistory.max_total_viewers", 3);
+
+
+                            options.set_preference('permissions.default.image',2 )
+                            ##禁用Flash
+                            options.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','false')
+
+                            # 火狐浏览器
+                            browser = webdriver.Firefox(executable_path='geckodriver', firefox_options=options)
+                            browser.set_page_load_timeout(10)
+                            browser.get(url)
+                            print(url)
+
+
+                            # if(count<maxStart):
+                            # praisebg =  WebDriverWait(browser, 10).until(
+                            #     EC.presence_of_element_located((By.ID, "praisebg")))
+                            # # # 点赞事件
+                            # praisebg.click()
+                            # browser.find_element_by_id("likebg").click()
                             # 显示统计数据
 
                             count = count+1
-                            print("url :"+url+" success"+str(count) )
+                            print("url :"+url+" success:"+str(count) )
+                            if(count >= max):
+                                mysqlCli.updateStatus(2,1)
                             redisClient.setUseStart(url,ip)
-                            redisClient.setSuccessStart(url,count+1)
-                            mysqlCli.updatestartNum(count+1,id)
+                            redisClient.setSuccessStart(url,count)
+                            mysqlCli.updatestartNum(str(count),str(id))
                         except Exception as e:
                             print(e)
                             print("浏览url出错")
                             break
                         finally:
-                            browser.close()
+                            if(browser !=None):
+                                browser.close()
                 else:
                     redisClient.deleteProxyData(ip)
-        except  :
-            print("异常")
+        except       Exception as e:
+            print( e)
             continue
 
 
--- start/mysqlCli.py
+++ start/mysqlCli.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 # 打开数据库连接
-db = MySQLdb.connect("47.94.196.111", "root", "11111", "iac", charset='utf8' )
+db = MySQLdb.connect("47.94.196.111", "root", "5998012", "iac", charset='utf8' )
 
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
@@ -12,7 +12,7 @@ cursor = db.cursor()
 # 查询ica 浏览信息
 
 def  icaUrls():
-    sql="select id ,url,start_num as startNum, max_browse_num as maxBrowseNum ,user_id as userId from iac_start where start_num<max_browse_num and delete_flag=0"
+    sql="select id ,url,start_num as startNum, max_browse_num as maxBrowseNum , max_start_num as maxStartNum ,user_id as userId from iac_start where start_num<max_browse_num and delete_flag=0"
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -22,9 +22,19 @@ def  icaUrls():
         return None
     # 关闭数据库连接
 
+#获取当前点赞数量
+def  getStartNumById(id):
+    sql="select id ,url,start_num as startNum, max_browse_num as maxBrowseNum , max_start_num as maxStartNum ,user_id as userId from iac_start where delete_flag=0 and id="+str(id)
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        return results[0][2]
+    except:
+        print ("查询iac信息出错")
+        return 0
 
-def updateStatus():
-    sql = "update iac_start set status = 1 where status=0 and delete_flag=0"
+def updateStatus(status,oldStatus):
+    sql = "update iac_start set status = "+str(status)+" where status="+str(oldStatus)+" and delete_flag=0"
     try:
         cursor.execute(sql)
         db.commit()
